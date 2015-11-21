7438695aa3ab98e1419e084d1b9296b4c525c064
alemjc <alemjc2204@gmail.com>
2015-11-21 14:01:49 -0500

    updated script file.

--- js/functions.js
+++ js/functions.js
@@ -1,15 +1,36 @@
 /**
  * Created by alemjc on 11/21/15.
  */
-$(document).ready(initilizer);
+var counter = 0;
+$(document).ready(initializer);
 
-function initilizer(){
-    $("#footerRow").children().on("click",addToBody);
+function initializer(){
+    console.log("initializer!!!!!!");
+    $("#footerRow").children().children().on("click",addToBody);
 }
 
 function addToBody(){
-    var element = $("<"+$(this).tagName+">").dblclick(deleteFromBody).hover(addAncestors,clearNavBar);
-    $("#body").append(element);
+
+    var element = $("<"+$(this).prop("tagName")+">");
+        element.dblclick(deleteFromBody);
+        element.hover(addAncestors,clearNavBar);
+    console.log("here");
+    if(counter === 0){
+        console.log("here1");
+        var row = $("<div class='row-fluid'>");
+        var col = $("<div class='col-md-3'>");
+        col.append(element);
+        row.append(col);
+        $("#body").append(row);
+    }
+    else{
+        console.log("here2");
+        $("#body").children().last().append($("<div class='col-md-3'>").append(element));
+
+    }
+
+    counter++;
+    counter%=3;
 
 }
 
